0a7ea92e9b7260d23502b9f0f418249ac6bac59e
Al3x4ndr0 <alexandro.amadeuz@gmail.com>
2020-05-13 19:14:03 -0500

    StatusDropdown w/<select>

--- src/components/Projects/ProjectForm.js
+++ src/components/Projects/ProjectForm.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import firebase from "firebase";
 import {useCollection} from "react-firebase-hooks/firestore";
-import useDropdown from './useDropdown.jsx';
+// import ReactSelect from "react-select";
+// import {Select} from "@material-ui/core";
 
 const ProjectForm = () => {
   const [data, loading, error] = useCollection(
@@ -9,62 +10,49 @@ const ProjectForm = () => {
     {
       snapshotListenOptions: { includeMetadataChanges: true },
     }
-    );
+  );
     
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [comments, setComments] = useState('');
-    const [status, setStatus] = useState('');
-    
-    const Status_List = ["New", "In Progress", "Test", "Canceled", "Implemented"];
-    const Dropdownlist = () => {
-      return (
-      <label>
-      <FirstDropdown />
-      <SecondDropdown />
-      <ThirdDropdown />
-      </label>
-      )
-    }
-  }
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [comments, setComments] = useState('');
+  const [status, setStatus] = useState('New')
   
   const onSubmit = (e) => {
     e.preventDefault();
-    if (name.length >= 4 && description.length >= 4 ){
-      firebase.firestore().collection('Project').add(
+      if (name.length >= 4 && description.length >= 4 ){
+        firebase.firestore().collection('Project').add(
         {name: name, description: description, comments: comments, status: status })
-        .then( async documentReference => {
+          .then( async documentReference => {
         console.log('document reference ID', documentReference.id);
-      })
-      .catch(error => {
-        console.log(error.message)
-      })
-    }
-    
+        })
+        .catch(error => {
+          console.log(error.message)
+        })
+      }
     return (
       <form onSubmit={onSubmit} className="form">
-      <label>Project Name </label>
-      <input type="text" required onChange={(event => setName(event.target.value))} minlength="4" maxlength="30" size="30"/>
-      <br/>
-      <label>Description </label>
-      <textarea  onChange={(event => setDescription(event.target.value))} minlength="4" maxlength="50" size="50"/>
-      <br/>
-      <label>Comments </label>
-      <textarea onChange={(event => setComments(event.target.value))}/>
-      <br/>
-
-      const Component = () => {
-          const [status, statusDropdown ] = useDropdown(" Status List ", " ", Status_List);          
-              return (
-              <statusDropdown onChange={(event => setStatus(event.target.value))}/>    
-              )
-      }
-      
-
-      <br/>
-      <input type="submit" value="Submit project"/>
-    </form>
-  )
+        <label>Project Name: </label>
+        <input type="text" required onChange={(event => setName(event.target.value))} minlength="4" maxlength="30" size="30"/>
+        <br/>
+        <label>Description: </label>
+        <textarea  onChange={(event => setDescription(event.target.value))} minlength="4" maxlength="50" size="50"/>
+        <br/>
+        <label>Comments: </label>
+        <textarea onChange={(event => setComments(event.target.value))}/>
+        <br/>
+        <label>Status: </label>
+        {/* <select> */}
+          {/* // onChange={(event => setStatus(event.target.value))}> */}
+            {/* <option value={"New"}>New</option> */}
+            {/* <option value={"In Progress"}>In Progress</option> */}
+            {/* <option value={"Test"}>Test</option> */}
+            {/* <option value={"Implemented"}>Implemented</option> */}
+            {/* <option value={"Canceled"}>Canceled</option> */}
+        {/* </select> */}
+        <br/>
+        <input type="submit" value="Submit project"/>
+          </form>
+    )
+  }
 }
-
 export default ProjectForm;
